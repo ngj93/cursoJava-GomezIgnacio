0be14268532e185cc7a444b17d457af05dfcddf2
ngj93 <101230386+ngj93@users.noreply.github.com>
2022-06-24 17:40:52 -0300

    Add files via upload

---
+++ miarchivo.js
@@ -0,0 +1,52 @@
+const Bitcoin = "20.506,59";
+const Ethereum = "1106,22";
+const Tether = "0,998987";
+const USDCoin = "1,00";
+const BNB = "214,96";
+const BinanceUSD = "0,999561";
+const Cardano = "0,491512";
+const XRP = "0,322990";
+const Solana = "35,10";
+const Dogecoin = "0,059903"; 
+const numeroingresado = "0"
+const dolar = "1"
+let resultado = "0";
+let valor = "0";
+let nuevaOperacion: false; 
+let operacion:"";
+
+
+
+function calcular(valormoneda){
+    let resultado = valor/valormoneda
+    return mostrarResultado
+
+}
+
+let mostrarResultado= cambiarMoneda()
+console.log(mostrarResultado)
+
+function elegirMoneda(){
+    switch(operacion){
+        case "Bitcoin":
+            resultado= cambiarMoneda(Bitcoin).toFixed(2)
+            alert("Te quedaran"+" "+resultado+" "+Bitcoin)
+            break;
+    }
+
+}
+
+function moneda(){
+do{
+    valor= Number(prompt("Ingrese un valor "));
+    operacion= prompt("A que moneda desea convertirlo?");
+    elegirMoneda()
+    nuevaOperacion= confirm ("A que otra moneda desea convertirlo?")
+} while (nuevaOperacion);
+}
+
+moneda()
+
+ 
+
+
