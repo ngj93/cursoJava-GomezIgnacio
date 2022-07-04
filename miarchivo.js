1716fb94f5ae85a6839e3856979548e3414bb74c
ngj93 <101230386+ngj93@users.noreply.github.com>
2022-07-03 21:37:24 -0300

    Add files via upload

--- miarchivo.js
+++ miarchivo.js
@@ -1,57 +1,52 @@
-const Bitcoin = "20.506,59";
-const Ethereum = "1106,22";
-const Tether = "0,998987";
-const USDCoin = "1,00";
-const BNB = "214,96";
-const BinanceUSD = "0,999561";
-const Cardano = "0,491512";
-const XRP = "0,322990";
-const Solana = "35,10";
-const Dogecoin = "0,059903"; 
-const numeroingresado = "0"
-const dolar = "1"
-let resultado = "0";
-let valor = "0";
-let nuevaOperacion = false; 
-let operacion = "";
+const Bitcoin = { nombre: "Bitcoin", precio: 20.506,59 } 
+const Ethereum = { nombre: "Ethereum", precio: 1106,22 }
+const Tether = { nombre: "Tether", precio: 0,998987 }
+let nuevaCompra = 'false';
 
+let compra = '';
+let moneda = '';
 
-
-function cambiarMoneda (valormoneda) {
-    let resultado = valor / valormoneda
-    return mostrarResultado
+function comprarCrypto (valormoneda) {
+    let mostrarResultado = compra / valormoneda
+    return mostrarResultado;
 
 }
 
-let mostrarResultado= cambiarMoneda()
-console.log(mostrarResultado)
+do {
+    compra = Number(prompt('cuanto quiere cambiar'));
+    moneda = prompt(
+        ` Que moneda quiere comprar? 
+          1. Bitcoin
+          2. Ethereum
+          3. Tether
+          
+        `
 
-function elegirMoneda(){
-    switch(operacion){
-        case "Bitcoin":
-            resultado= cambiarMoneda(Bitcoin).toFixed(2)
-            alert("Te quedaran"+" "+resultado+" "+Bitcoin)
-            break;
+          
+          
+
+    );
+    switch (moneda) {
+        case '1':
+                resultado = comprarCrypto(Bitcoin);
+                alert('compraste ${resultado} btc');
+                break;
+        case '2':
+                resultado = comprarCrypto(Ethereum);
+                alert('compraste ${resultado} eth');
+                break;
+        case '3':
+                resultado = comprarCrypto(Tether);
+                alert('compraste ${resultado} tether');
+                break;
+        default:
+                alert('error de ingreso');
+                valor = 0;
+                moneda = '';
+                break;
     }
-
-}
-
-function moneda(){
-do{
-    valor= Number(prompt("Ingrese un valor "));
-    operacion= prompt("A que moneda desea convertirlo?");
-    elegirMoneda()
-    nuevaOperacion= confirm ("A que otra moneda desea convertirlo?")
-} while (nuevaOperacion);
-}
-
-moneda()
-
-
-
-let criptos = ["Bitcoin", "Ethereum", "Tether", "USDCoin", "BNB", "BinanceUSD"]
-
-console.log(criptos.length)
+        nuevaCompra = confirm('Quieres realizar otra operacion?');
+}while (nuevaCompra);
 
  
 
